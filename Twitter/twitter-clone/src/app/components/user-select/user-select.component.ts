62b08a46ce6a0397258acb704577008782cbf00b
ryan <ryannesh2376@gmail.com>
2025-03-28 10:22:37 +0300

    projStart

---
+++ Twitter/twitter-clone/src/app/components/user-select/user-select.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-select',
+  imports: [],
+  templateUrl: './user-select.component.html',
+  styleUrl: './user-select.component.css'
+})
+export class UserSelectComponent {
+
+}
